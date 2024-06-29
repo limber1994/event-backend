e2d396a6e2ddd1662cc832fe9c908eb93da3fa97
limber quispe caceres <limbe.set@gmail.com>
2024-06-29 15:35:33 -0500

    terminado

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\EventController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
+Route::get('events', [EventController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('events', [EventController::class, 'index']);
+    
     Route::post('events', [EventController::class, 'store']);
     Route::put('events/{event}', [EventController::class, 'update']);
     Route::delete('events/{event}', [EventController::class, 'destroy']);
